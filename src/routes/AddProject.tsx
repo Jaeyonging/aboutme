6815e6a5212e4312dfd31acf286107075eb8a462
Jaeyonging <wodyd1318@naver.com>
2024-04-02 18:17:52 +0900

    add project

--- src/routes/AddProject.tsx
+++ src/routes/AddProject.tsx
@@ -1,13 +1,84 @@
-import React from 'react'
-import { NavBar } from '../components/NavBar'
+import React, { useState } from 'react';
+import { NavBar } from '../components/NavBar';
+import { Projects } from '../types/types';
 
 export const AddProject = () => {
+    const [formData, setFormData] = useState<Projects>({
+        id: '',
+        date: '',
+        gameurl: '',
+        hashtags: [],
+        imgurl: '',
+        password: '',
+        project: ''
+    });
 
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const { name, value } = e.target;
+        setFormData(prevState => ({
+            ...prevState,
+            [name]: value
+        }));
+    };
+
+    const handleHashtagsChange = (e: React.ChangeEvent<HTMLInputElement>, index: number) => {
+        const newHashtags = [...formData.hashtags];
+        newHashtags[index] = e.target.value;
+        setFormData(prevState => ({
+            ...prevState,
+            hashtags: newHashtags
+        }));
+    };
+
+    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        console.log(formData);
+    };
 
     return (
         <>
-            <NavBar></NavBar>
-            <div>AddProject</div>
+            <NavBar />
+            <form className="add-project-form" onSubmit={handleSubmit}>
+                <div className="form-group">
+                    <label>ID:</label>
+                    <input className='project-text' type="text" name="id" value={formData.id} onChange={handleChange} />
+                </div>
+
+                <div className="form-group">
+                    <label>Date:</label>
+                    <input className='project-text' type="text" name="date" value={formData.date} onChange={handleChange} />
+                </div>
+
+                <div className="form-group">
+                    <label>Game URL:</label>
+                    <input className='project-text' type="text" name="gameurl" value={formData.gameurl} onChange={handleChange} />
+                </div>
+
+                <div className="form-group">
+                    <label>Hashtags:</label>
+                    {formData.hashtags.map((tag, index) => (
+                        <input key={index} type="text" value={tag} onChange={(e) => handleHashtagsChange(e, index)} />
+                    ))}
+                    <button className='submit-button' onClick={() => setFormData(prevState => ({ ...prevState, hashtags: [...prevState.hashtags, ''] }))}>Add Hashtag</button>
+                </div>
+
+                <div className="form-group">
+                    <label>Image URL:</label>
+                    <input className='project-text' type="text" name="imgurl" value={formData.imgurl} onChange={handleChange} />
+                </div>
+
+                <div className="form-group">
+                    <label>Password:</label>
+                    <input className='project-text' type="text" name="password" value={formData.password} onChange={handleChange} />
+                </div>
+
+                <div className="form-group">
+                    <label>Project:</label>
+                    <input className='project-text' type="text" name="project" value={formData.project} onChange={handleChange} />
+                </div>
+
+                <button className="submit-button" type="submit">Submit</button>
+            </form>
         </>
-    )
-}
+    );
+};
