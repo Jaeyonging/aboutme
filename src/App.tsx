be1c448203841c1555f72285cb36dc30911bdf55
jaeyonging <wodyd1318@naver.com>
2024-06-27 16:28:44 +0900

    fix: login

--- src/App.tsx
+++ src/App.tsx
@@ -11,55 +11,31 @@ import { Contact } from "./routes/Contact";
 import { Login } from "./routes/Login/Login";
 import { Learned } from "./routes/Learned";
 import { Master } from "./routes/Master";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { RootState } from "./store/configureStore";
 import { AddProject } from "./routes/AddProject";
 import { AddLearn } from "./routes/AddLearn";
 import { Mbti } from "./routes/Mbti"
 import ReactGA from 'react-ga4';
+import { LoginSuccess } from "./store/userSlice";
 
 const TRACKING_ID = import.meta.env.VITE_GA_PROPERTYID;
 ReactGA.initialize(TRACKING_ID);
 
 function App() {
+  const dispatch = useDispatch()
   const location = useLocation();
   console.log("start")
 
   useEffect(() => {
     ReactGA.set({ page: location.pathname });
     ReactGA.send('pageview');
+    dispatch(LoginSuccess())
   }, [location]);
-
-  // const [xy, setXY] = useState({ x: 0, y: 0 });
-  // const mainRef = useRef<HTMLDivElement | null>(null);
-
-  // const xyHandler = (e: { clientX: number; clientY: number; }) => {
-  //   const mouseX = e.clientX - 11;
-  //   const mouseY = e.clientY - 11;
-  //   if (mainRef.current) {
-  //     const rect = mainRef.current.getBoundingClientRect();
-  //     const offsetX = mouseX - rect.left;
-  //     const offsetY = mouseY - rect.top;
-  //     setXY({ x: offsetX, y: offsetY });
-  //   }
-  // };
-
-  // useEffect(() => {
-  //   document.addEventListener('mousemove', xyHandler);
-  //   console.log("hi")
-  //   return () => {
-  //     document.removeEventListener('mousemove', xyHandler);
-  //   };
-  // }, []);
 
   const userInfo = useSelector((state: RootState) => state.userInfo)
   return (
     <>
-      {/* <div className="main" ref={mainRef}>
-        <div className='pointer' style={{
-          transform: `translate(${xy.x}px, ${xy.y}px)`
-        }}>
-        </div> */}
 
       <Suspense fallback={<div>로딩중</div>}>
 
